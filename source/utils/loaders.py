78f4c2def1705343ea3fe1405ad78a4d1fdc9ba3
X-Aleksey-X <TheHentWill@gmail.com>
2020-12-24 18:08:48 +0200

    feat: добавил загрузчик когов

---
+++ source/utils/loaders.py
@@ -0,0 +1,21 @@
+"""
+Утилита для загрузки чего либо.
+"""
+
+from discord.ext import commands
+
+
+def load_cogs(bot: commands.Bot, cogs_list: list, cogs_path: str) -> str:
+    """
+    Функция для загрузки когов бота.
+
+    :param bot: Бот, для которого загружаются коги.
+    :param cogs_list: Список когов, которые загружаются.
+    :param cogs_path: Путь к когам, которые загружаются.
+    """
+    
+    for cog in cogs_list:
+        try:
+            bot.load_extension(name = f"{cogs_path}.{cog}")
+        except (ImportError, AttributeError) as error:
+            print(f"При загрузке кога {cog} произошла ошибка:\n{error}")
